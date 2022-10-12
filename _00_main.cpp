4f74cb8cef74487bd4835a1165f665c7616055fa
沈彬峰 <shen.binfeng@yalla.live>
2022-10-12 11:55:46 +0800

    @feat add return 0 注释

--- _00_main.cpp
+++ _00_main.cpp
@@ -11,6 +11,8 @@ int main() {
     printHelloWorld();
     _02_readinput::readInput(); // 暴露方法的第二种方式
     _00_main::case03();
+
+    return 0; //0代表程序正常退出 1/-1代表程序异常退出
 }
 
 void _00_main::case03() {
